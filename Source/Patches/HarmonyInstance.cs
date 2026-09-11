86bb435051306cd28ac42878e6e06350108c93cb
mantuoluo911 <mantuoluo020911@gmail.com>
2026-09-12 01:29:52 +0800

    refactor(harmony): 使用规范的 PatchAll 方法

---
+++ Source/Patches/HarmonyInstance.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+using HarmonyLib;
+using System.Reflection;
+
+namespace RimTalk.Memory.Patches;
+
+//Setting the Harmony instance
+[StaticConstructorOnStartup]
+public class Main
+{
+    static Main()
+    {
+        var harmony = new Harmony("cj.rimtalk.expandmemory");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
